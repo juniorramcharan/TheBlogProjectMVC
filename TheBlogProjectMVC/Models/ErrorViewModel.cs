a94398869e99bb9c4cc812597f170b86eae22fa1
Charles Ramcharan <charlesramcharan@yahoo.com>
2021-08-30 14:45:18 -0400

    Initial commit

---
+++ TheBlogProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheBlogProjectMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
